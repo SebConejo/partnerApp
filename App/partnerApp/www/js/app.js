c82dc48b2e1844fe2c622e2b563d02dbdad708aa
Bruno Pérez <bruno.perez@buddyweb.fr>
2016-11-26 23:27:30 +0100

    add CustomerService and rename states

--- App/partnerApp/www/js/app.js
+++ App/partnerApp/www/js/app.js
@@ -5,51 +5,49 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives','app.services',])
+angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives', 'app.services', 'restangular'])
 
-.config(function($ionicConfigProvider, $sceDelegateProvider){
-  
-    $ionicConfigProvider.backButton.text('').previousTitleText(false);
-  
+    .config(function ($ionicConfigProvider, $sceDelegateProvider) {
 
-  $sceDelegateProvider.resourceUrlWhitelist([ 'self','*://www.youtube.com/**', '*://player.vimeo.com/video/**']);
+        $ionicConfigProvider.backButton.text('').previousTitleText(false);
 
-})
+        $sceDelegateProvider.resourceUrlWhitelist(['self', '*://www.youtube.com/**', '*://player.vimeo.com/video/**']);
+    })
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.directive('disableSideMenuDrag', ['$ionicSideMenuDelegate', '$rootScope', function($ionicSideMenuDelegate, $rootScope) {
-    return {
-        restrict: "A",  
-        controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
-
-            function stopDrag(){
-              $ionicSideMenuDelegate.canDragContent(false);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
             }
-
-            function allowDrag(){
-              $ionicSideMenuDelegate.canDragContent(true);
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
             }
+        });
+    })
 
-            $rootScope.$on('$ionicSlides.slideChangeEnd', allowDrag);
-            $element.on('touchstart', stopDrag);
-            $element.on('touchend', allowDrag);
-            $element.on('mousedown', stopDrag);
-            $element.on('mouseup', allowDrag);
+    .directive('disableSideMenuDrag', ['$ionicSideMenuDelegate', '$rootScope', function ($ionicSideMenuDelegate, $rootScope) {
+        return {
+            restrict: "A",
+            controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
 
-        }]
-    };
-}])
+                function stopDrag() {
+                    $ionicSideMenuDelegate.canDragContent(false);
+                }
+
+                function allowDrag() {
+                    $ionicSideMenuDelegate.canDragContent(true);
+                }
+
+                $rootScope.$on('$ionicSlides.slideChangeEnd', allowDrag);
+                $element.on('touchstart', stopDrag);
+                $element.on('touchend', allowDrag);
+                $element.on('mousedown', stopDrag);
+                $element.on('mouseup', allowDrag);
+
+            }]
+        };
+    }])
--- App/partnerApp/www/js/controllers.js
+++ App/partnerApp/www/js/controllers.js
@@ -1,5 +1,5 @@
 angular.module('app.controllers', [])
-  
+
 .controller('page1Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -8,7 +8,7 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('partnerHomePageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
